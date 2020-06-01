e9d408e3aa2899149a13f699638321607b281704
paulinhovba <falecompaulinhomonteiro@gmail.com>
2020-06-01 12:11:37 -0300

    Commit Inicial Projeto Frontend DEMO

---
+++ js/main.js
@@ -0,0 +1,60 @@
+(function() {
+	var triggerBttn = document.getElementById( 'trigger-overlay' ),
+		overlay = document.querySelector( 'div.overlay' ),
+		closeBttn = overlay.querySelector( 'button.overlay-close' );
+		transEndEventNames = {
+			'WebkitTransition': 'webkitTransitionEnd',
+			'MozTransition': 'transitionend',
+			'OTransition': 'oTransitionEnd',
+			'msTransition': 'MSTransitionEnd',
+			'transition': 'transitionend'
+		},
+		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+		support = { transitions : Modernizr.csstransitions };
+
+	function toggleOverlay() {
+		if( classie.has( overlay, 'open' ) ) {
+			classie.remove( overlay, 'open' );
+			classie.add( overlay, 'close' );
+			var onEndTransitionFn = function( ev ) {
+				if( support.transitions ) {
+					if( ev.propertyName !== 'visibility' ) return;
+					this.removeEventListener( transEndEventName, onEndTransitionFn );
+				}
+				classie.remove( overlay, 'close' );
+			};
+			if( support.transitions ) {
+				overlay.addEventListener( transEndEventName, onEndTransitionFn );
+			}
+			else {
+				onEndTransitionFn();
+			}
+		}
+		else if( !classie.has( overlay, 'close' ) ) {
+			classie.add( overlay, 'open' );
+		}
+	}
+
+	triggerBttn.addEventListener( 'click', toggleOverlay );
+	closeBttn.addEventListener( 'click', toggleOverlay );
+})();
+
+// Hamburger
+
+const btn = document.getElementById("trigger-overlay");
+const lines = btn.querySelectorAll(".line");
+const cls = { open: "open", close: "close" };
+let btnClass = cls.open;
+
+btn.addEventListener("click", () => {
+  if (btn.classList.contains(cls.open)) {
+    btn.classList.remove(btnClass);
+    btnClass = cls.close;
+  } else if (btn.classList.contains(cls.close)) {
+    btn.classList.remove(btnClass);
+    btnClass = cls.open;
+  }
+
+  void btn.offsetWidth;
+  btn.classList.add(btnClass);
+});
